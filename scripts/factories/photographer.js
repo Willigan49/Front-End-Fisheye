24f051cf79305d8e96c4eec1e198cbc8e92e39ec
ggernigon <ggernigon@puydufou.com>
2022-02-21 14:02:07 +0100

    home done

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
+  const photographerPage = `../../photographer.html`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    link.setAttribute("href", photographerPage);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const locationText = document.createElement("p");
+    locationText.classList.add("location");
+    locationText.textContent = city + ", " + country;
+    const taglineText = document.createElement("p");
+    taglineText.classList.add("tagline");
+    taglineText.textContent = tagline;
+    const priceText = document.createElement("p");
+    priceText.classList.add("price");
+    priceText.textContent = price + "€/jour";
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(locationText);
+    article.appendChild(taglineText);
+    article.appendChild(priceText);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
